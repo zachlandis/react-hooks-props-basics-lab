2142c9534996be77d28794ea07b695f81a82edbb
Zach <zachlandis91@gmail.com>
2023-02-08 13:55:25 -0700

    commit tree 9ea57ecfc47382d2c5fdb3fe242a93a39a4ddc7e

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,14 @@
 import React from "react";
+import user from "../data/user";
+import Links from './Links'
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {props.bio ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={user.links.github} linkedin={user.links.linkedin}/>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import React from "react";
 function Home(props) {
   return (
     <div id="home">
+      {/* {console.log(props)} */}
       <h1 style={{ color: props.color }}>
         {props.name} is a Web Developer from {props.city}
       </h1>
